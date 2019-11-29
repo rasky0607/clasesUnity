817273848182299b00ffb67fd859e63c8820ffd2
Pablo Lopez <esviasalpa@gmail.com>
2019-11-29 12:34:20 +0100

    Ejemplo fuerza de torque y desplaczamiento usando RigiBody

---
+++ Tema4/FuerzaDeTorque/Assets/bola.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+/*Script aplicado a la bola para aplicarle una fuerza de torque*/
+public class bola : MonoBehaviour {
+
+    public float _fuerzaTorque=50F;
+    Vector3 _aplicarFuerza;
+
+    public float _fuerzaImpulso = 10F;
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        /*El valor va ser positivo o negativo, segun la tecla que se pulse,
+         * ya que Horizontaldevuelve 1 o -1 segun que tecla es pulsada si felcha izqu o ferecha*/
+        _aplicarFuerza = Vector3.up * _fuerzaTorque * Time.deltaTime*Input.GetAxis("Horizontal");
+        //Accede al componente del rigibody y aplicamos la fuerza de torque
+        GetComponent<Rigidbody>().AddTorque(_aplicarFuerza, ForceMode.Force);
+
+        //Nota: GetButtonDown devuelve verdad o falso segun si se pulso y GetAxis 0 1 o -1 segun si se pulso, no se pulso o si se pulso  dercha o izquierda
+        //Fuerza de impulso "Aplicamos una fuerza de impulso sobre el eje Z
+        if (Input.GetButtonDown("Vertical"))
+            GetComponent<Rigidbody>().AddForce(Vector3.forward * _fuerzaImpulso * Time.deltaTime,ForceMode.Impulse);
+
+        //Posicionar un objeto con el componente Rigibody usando su metodo position
+	}
+}
---
+++ Tema4/FuerzaDeTorque/Assets/cubo.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+/*Posicionar un objeto con el componente Rigibody usando su metodo position*/
+public class cubo : MonoBehaviour {
+    Quaternion q1;
+    public Vector3 _posicionInicial;
+	// Use this for initialization
+	void Start () {
+        _posicionInicial = Vector3.zero;
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        GetComponent<Rigidbody>().position = _posicionInicial;
+       
+        
+    }
+}
