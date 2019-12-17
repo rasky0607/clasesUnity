44f0873f9df98ddda2667fcae50ba2cf3a7dd64d
Pablo Lopez <esviasalpa@gmail.com>
2019-12-17 14:42:26 +0100

    añadida rotaciones con clase quartenion

---
+++ Tema4/rotaciones/Assets/avionEuler.cs
@@ -0,0 +1,56 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+/*Asignado al avion euler para controlar sus rotacion de ladeo
+ * Este ejemplo se usara el metodo Euler con Vectores3 para ver como 
+ * el uso de vectores3 con Euler en ocasiones puede dar error con las rotaciones
+ * ya que al coincidr los angulos, puede haber una perdida de rotacion de un grado,
+ * por lo que podria volverla rotacion loca
+ */
+public class avionEuler : MonoBehaviour {
+
+    public float _velocidadRotacion = 90F;
+
+    //Estos miembros se heredan en la otra clase, de hay que sean protegidos.
+    protected float _rotadcionEjeX;
+    protected float _rotadcionEjeY;
+    protected float _rotadcionEjeZ;
+
+    // Use this for initialization
+    void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        //Asignar los valores en funcion de la tecla pulsada
+        _rotadcionEjeZ = Input.GetAxis("Vertical");
+        _rotadcionEjeY = Input.GetAxis("Horizontal");
+        //Usaremos las teclas y z y x 
+        _rotadcionEjeX = Input.GetAxis("EjeX");//Nueva entrada creada en el Input Manager
+
+        AplicarRotacion();
+
+    }
+
+    /*Virtual para que se  pueda sobreescribir, ya quelo vamos a compartir con otra clase*/
+    protected virtual void AplicarRotacion() {
+        Vector3 rotacionActual = transform.localEulerAngles; //Rotacion desde la que vamos a empezar
+        //Modifico la rotacion actual, con los valores que lea desde el teclado
+        rotacionActual.x += _rotadcionEjeX * _velocidadRotacion * Time.deltaTime;
+        rotacionActual.y += _rotadcionEjeY * _velocidadRotacion * Time.deltaTime;
+        rotacionActual.z += _rotadcionEjeZ * _velocidadRotacion * Time.deltaTime;
+
+        //Aplicamos la rotaciones al transform
+        transform.eulerAngles = rotacionActual;
+
+    }
+
+    void OnGUI() {
+        GUI.color = Color.blue;
+        //Mostramos la rotacion
+        GUILayout.Label("Rotacion X " + _rotadcionEjeX);
+        GUILayout.Label("Rotacion Y " + _rotadcionEjeY);
+        GUILayout.Label("Rotacion Z " + _rotadcionEjeZ);
+    }
+}
---
+++ Tema4/rotaciones/Assets/avionQuaternion.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+/*Esta clase hereda de avionEuler y como esta hereda MonoBehaviour , tiene toda la funcionalidad de unity
+ En esta clas eno usamos los Vectore3 si no que usamos directamente un Quartenion que hemos creado, para evitar
+ el bloqueo que tiene el otro avion con el script avionEuler*/
+public class avionQuaternion : avionEuler {
+
+    
+
+    
+    protected override void AplicarRotacion()
+    {
+        Vector3 _rotacionActual = Vector3.zero;
+        _rotacionActual.x = _rotadcionEjeX;
+        _rotacionActual.y = _rotadcionEjeY;
+        _rotacionActual.z = _rotadcionEjeZ;
+
+        Quaternion rotacion = Quaternion.Euler(_rotacionActual * _velocidadRotacion * Time.deltaTime);
+        //IMPORTANTE: Sumamos la rotacion actual con la resultante, para obtener el producto
+        transform.rotation *= rotacion;
+
+    }
+
+}
