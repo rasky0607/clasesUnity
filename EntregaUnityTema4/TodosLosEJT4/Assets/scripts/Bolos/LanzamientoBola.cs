de2758a54d4f9ed5bbcb8e93022e168005803946
Pablo Lopez <pablolopez@MacBook-Air-de-Pablo.local>
2020-02-02 22:40:30 +0100

    Juego de bolos casi listo

---
+++ EntregaUnityTema4/TodosLosEJT4/Assets/scripts/Bolos/LanzamientoBola.cs
@@ -0,0 +1,86 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class LanzamientoBola : MonoBehaviour {
+
+	public Image img;
+	Rigidbody rb;
+	public Slider sliderFuerza;//muestra el usuario la fuerza con la que va lanzar
+	public Text textFuerza;//Especifica la cantidad de fuerza que se muestra en el slider
+	float fuerzaLanzamiento = 0;
+	public Slider sliderPrecision;//Muestra la posibilidad y cantidad de fuerza de torque que se le aplica a la bola
+	float fuerzaTorqueVaraible = 0;
+	public Text textPrecision;
+	bool subiendoValorTorque = true; //Bandera que indica  al slider de Precision cuando la esta incrementando la fuerzaTorqueVaraible hasta 5 y cuando la esta decrementando hasta-5
+	float velocidadDesplazHorizontal = 0.2F;//velocidad de desplazamiento horizontal de la bola
+	bool funcionamientoSliderPrecision = true;
+	// Use this for initialization
+	void Start () {
+		rb = GetComponent<Rigidbody>();
+		sliderFuerza.value = fuerzaLanzamiento;
+		textFuerza.text = "0";
+		sliderPrecision.value = fuerzaTorqueVaraible;
+		textPrecision.text = fuerzaTorqueVaraible.ToString();
+        
+	}
+    private void FixedUpdate()
+    {
+		
+	}
+
+    // Update is called once per frame
+    void Update () {
+
+        if(funcionamientoSliderPrecision)
+		    FuncionamientoSliderPrecision();
+
+		if (Input.GetKey(KeyCode.Space)) //Mientras la Space este pulsada, acumulamos fuerza
+		{
+			funcionamientoSliderPrecision = false;//el SliderPrecision, se para
+            //cargamos la fuerza hasta un maximo de 10
+            if(fuerzaLanzamiento<10)
+			    fuerzaLanzamiento++;
+			sliderFuerza.value = fuerzaLanzamiento;
+			textFuerza.text = sliderFuerza.value.ToString();
+			//Recogemos la fuerza de torque al pulsar la tecla espacio que mas tarde aplicaremos al lanzar la bola
+			fuerzaTorqueVaraible = sliderPrecision.value;
+		}
+
+		//Cuando soltamos la Space, lanzamos la bola
+		if (Input.GetKeyUp(KeyCode.Space))
+		{		
+			rb.AddForce(new Vector3(transform.position.x, transform.position.y, transform.position.z * -fuerzaLanzamiento), ForceMode.Impulse);
+			rb.AddTorque(new Vector3(transform.position.x, transform.position.y, transform.position.z* fuerzaTorqueVaraible), ForceMode.Impulse);
+        }
+
+		//Movimiento de la bola en  direccion horizontal (en eje X)(Solo se puede desplazar la bola mientras este parada)
+        if(fuerzaLanzamiento ==0)
+		    transform.Translate(Input.GetAxis("Horizontal")*velocidadDesplazHorizontal, 0,0);
+		
+	}
+
+	public void FuncionamientoSliderPrecision() {
+		Debug.Log("Estado flag: " + subiendoValorTorque + " Valor: " + sliderPrecision.value);
+		if (sliderPrecision.value > -5 && subiendoValorTorque)//Empezamos a decrementar el valor (la bola  va hacia la izquierda) 
+		{
+			//Solo decrementamos el valor
+			sliderPrecision.value--;
+			textPrecision.text = sliderPrecision.value.ToString();
+
+			if (sliderPrecision.value == -5)//activamos  bandera, para el cambio de sentido
+				subiendoValorTorque = false;//A partir de ahora empieza a subir hasta 5			
+
+		}
+		else if (sliderPrecision.value < 5 && !subiendoValorTorque)//Empezamos a incrementar el valor (la bola  va hacia la derecha) 
+		{
+			//solo aumentamos el valor
+			sliderPrecision.value++;
+			textPrecision.text = sliderPrecision.value.ToString();
+
+			if (sliderPrecision.value == 5)//activamos bandera para el cambio de sentido
+				subiendoValorTorque = true;//A partir de ahora empieza a bajara hasta  -5
+		}
+	}
+}
---
+++ EntregaUnityTema4/TodosLosEJT4/Assets/scripts/Bolos/bolosCaidos.cs
@@ -0,0 +1,90 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+public class bolosCaidos : MonoBehaviour {
+
+	public Image bolo1;
+	public Image bolo2;
+	public Image bolo3;
+	public Image bolo4;
+	public Image bolo5;
+	public Image bolo6;
+	public Image bolo7;
+	public Image bolo8;
+	public Image bolo9;
+	public Image bolo10;
+	// Use this for initialization
+	void Start () {
+		bolo1 = GameObject.Find("ImageBolo1").GetComponent<Image>();
+		/*bolo1.sprite = Resources.Load<Sprite>("puntacion/boloPuntuacion");//Cargamos la imagen
+		bolo2.sprite = Resources.Load<Sprite>("puntacion/boloPuntuacion");//Cargamos la imagen
+		bolo3.sprite = Resources.Load<Sprite>("puntacion/boloPuntuacion");//Cargamos la imagen
+		bolo4.sprite = Resources.Load<Sprite>("puntacion/boloPuntuacion");//Cargamos la imagen
+		bolo5.sprite = Resources.Load<Sprite>("puntacion/boloPuntuacion");//Cargamos la imagen
+		bolo6.sprite = Resources.Load<Sprite>("puntacion/boloPuntuacion");//Cargamos la imagen
+		bolo7.sprite = Resources.Load<Sprite>("puntacion/boloPuntuacion");//Cargamos la imagen
+		bolo8.sprite = Resources.Load<Sprite>("puntacion/boloPuntuacion");//Cargamos la imagen
+		bolo9.sprite = Resources.Load<Sprite>("puntacion/boloPuntuacion");//Cargamos la imagen
+		bolo10.sprite = Resources.Load<Sprite>("puntacion/boloPuntuacion");//Cargamos la imagen*/
+
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+
+    /*Sensores:
+     * 1ºla cabeza de lso bolos, estan marcadas
+     * como isTrigger y tienen un tag que identifican el numero del bolo, asi como en el marcador superior derecho
+     2ºUn disparador llamado" DisparadorCaidaBolos" con un rigiBody que no es mas que un cubo al que se le desmarco la propiedad Mesh para que no se vea,
+     este es el que tiene asociado este Script "bolosCaidos.cs" ,cuandola cabeza de un bolo sale del colider de este, es identificado por el tag*/
+    private void OnTriggerExit(Collider other)
+    {
+		Debug.Log("AQUI TOY");
+		switch (other.tag) {
+			case "bolo1":
+				Debug.Log("Bolo 1 CAIDO!");
+                
+                bolo1.sprite = Resources.Load<Sprite>("aspa");//Cargamos la imagen
+				break;
+			case "bolo2":
+				Debug.Log("Bolo 2 CAIDO!");
+				bolo2.sprite = Resources.Load<Sprite>("aspa");//Cargamos la imagen
+				break;
+			case "bolo3":
+				Debug.Log("Bolo 3 CAIDO!");
+				bolo3.sprite = Resources.Load<Sprite>("aspa");//Cargamos la imagen
+				break;
+			case "bolo4":
+				Debug.Log("Bolo 4 CAIDO!");
+				bolo4.sprite = Resources.Load<Sprite>("aspa");//Cargamos la imagen
+				break;
+			case "bolo5":
+				Debug.Log("Bolo 5 CAIDO!");
+				bolo5.sprite = Resources.Load<Sprite>("aspa");//Cargamos la imagen
+				break;
+			case "bolo6":
+				Debug.Log("Bolo 6 CAIDO!");
+				bolo6.sprite = Resources.Load<Sprite>("aspa");//Cargamos la imagen
+				break;
+			case "bolo7":
+				Debug.Log("Bolo 7 CAIDO!");
+				bolo7.sprite = Resources.Load<Sprite>("aspa");//Cargamos la imagen
+				break;
+			case "bolo8":
+				Debug.Log("Bolo 8 CAIDO!");
+				bolo8.sprite = Resources.Load<Sprite>("aspa");//Cargamos la imagen
+				break;
+			case "bolo9":
+				Debug.Log("Bolo 9 CAIDO!");
+				bolo9.sprite = Resources.Load<Sprite>("aspa");//Cargamos la imagen
+				break;
+			case "bolo10":
+				Debug.Log("Bolo 10 CAIDO!");
+				bolo10.sprite = Resources.Load<Sprite>("aspa");//Cargamos la imagen
+				break;		
+		}
+    }
+}
