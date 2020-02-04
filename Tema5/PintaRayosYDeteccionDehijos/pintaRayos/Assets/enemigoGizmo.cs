e9d0b9d8e2c22df292283c2e50ad92689ba043ae
Pablo Lopez <esviasalpa@gmail.com>
2020-02-04 14:39:04 +0100

    comiezo tema5

---
+++ Tema5/PintaRayosYDeteccionDehijos/pintaRayos/Assets/enemigoGizmo.cs
@@ -0,0 +1,64 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+/**Este script, pretende pintaruna linea, entre los enemigos o cubos en este caso mas cercanos y el jugador o capsula, */
+/**Otro ejemplo sobre este proyecto, es la de localizar los hijos de un objeto padre*/
+[ExecuteInEditMode]//esta etiqueta nos permite  ejecutar el script en el modo diseño, no en la ventana de game, para ver los gizmos, sin tener que ejecutarlo
+public class enemigoGizmo : MonoBehaviour {
+    public float range = 2F;//radio 2 es decir diametro 4 ,este es utilizado para marcar un gizmo redondo al rededor del enemigo marcando su zona de influencia.
+    List<GameObject> _enemigos=null;
+    public Transform _padre;
+
+	// Use this for initialization
+	void Start () {
+		//optener todos los enemigos
+        _enemigos = new List<GameObject>(GameObject.FindGameObjectsWithTag("enemigo"));//recoger la lista de todos los objetos que tiene el tag "enemigo"
+
+        //Esto demuestra que un transform padre esta formado por una coleccion de los transform de todos sus hijos
+        foreach (Transform item in _padre.transform)
+        {
+            Debug.Log("Localizado: "+item.gameObject.name);
+        }
+
+	}
+	
+	// Update is called once per frame
+	void Update () {
+       
+        //transform.Find("objetosEnemigos"); //encuentra el primer hijo  de este padre
+
+        
+
+    }
+
+    private void OnDrawGizmos() {
+        if (_enemigos == null || _enemigos.Count == 0)
+            return;
+
+        Gizmos.color = Color.blue;
+        float distanciaActual = float.MaxValue;//distancia maxima que hay
+        GameObject _enemigocercano = null;
+
+        //comprobamos la distancia
+        foreach (var item in _enemigos)
+        {
+            float distancia = Vector3.Distance(transform.position, item.transform.position); //distancia entre el enemigo y el jugador
+
+            if (distancia < distanciaActual)
+            {
+                distanciaActual = distancia;
+                _enemigocercano = item;
+            }
+
+           
+        }
+
+        if (_enemigocercano != null)//si es distinto d enull, dibujamos
+        {
+            Gizmos.DrawLine(transform.position, _enemigocercano.transform.position);
+        }
+
+    }
+
+}
---
+++ Tema5/ejemplo1Tema5/Assets/miscript.cs
@@ -0,0 +1,51 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+//añadido para  indicar que es serializable  la estructura ( en este cason o lo añadimos, por que usamos el nombre cualificativo [System.Serializable]
+
+//[ExecuteInEditMode]//ejecuta  solo determinadas partes del scrit
+//[HelpURL("www/iesportadaalta.com")] //Redirige ala hora de darle al librito de la ayuda del inspector, en lugar de la url por defecto de unity
+//[DisallowMultipleComponent]//Indica que este script es unico, lo que quiere decir que solo se puede arrastrar sobre un mismo GameObject una unica vez, ya que por defecto podemos asignar el mismo script al mismo GameObject  varias veces
+
+//[RequireComponent(typeof(Rigidbody))]//Esta etiqueta exige que el componente al que este asociado este script, tengaun RigiBody
+
+//creacion de structura
+[System.Serializable]
+ public struct DatosJugador {
+    public string _nombre;
+    public int _puntos;
+    //etc..
+
+}
+public class miscript : MonoBehaviour {
+
+    public DatosJugador datos;
+    public float Velocidad = 0;
+    public string _pulsame = "Pulsame";//Si nos fijamos en el  ispector esta variable no tiene  guion bajo
+    [HideInInspector] //Esta etiqueta  oculta sta variable en el inspector a pesar de ser publica, pero podemos acceder a ella desde otros script.
+    public GameObject _enemigo;
+
+    [Range(1,5)]//Esta etiqueta indica que esta variable solo puede tener valores entre 0 y 5 y en el inspector a parecera como un barrita, como el value de el slider
+    public float numero;
+    [Range(1,Mathf.PI)]
+    public double _nVidas;
+
+    [Multiline (3)]//esta etiqueta permite aun string, tener hasta 3 lineas en el entorno grafico y el inspector 
+    public string _texto = "En un lugar de la mancha";
+
+    [SerializeField]//Permite mostrar en el inspector una variable privada
+    private bool _Accion;
+
+    [Tooltip("Representa el tiempo que dura un enemigo")]//toolptip de ayuda para el inspector  para cuando  el nombre de una varaible que vemos en el ispector no es muy descriptivo
+    public int _tVDeVidaEnemigo = 5;
+
+    // Use this for initialization
+    void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+}
